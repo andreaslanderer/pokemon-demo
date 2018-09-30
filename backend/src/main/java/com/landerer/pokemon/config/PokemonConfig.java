5e649f5211cfef09539839b1bb15128b4705842e
Andreas Landerer <andreas.landerer@gmail.com>
2018-09-30 11:47:08 +0200

    Just some smaller changes

--- backend/src/main/java/com/landerer/pokemon/config/PokemonConfig.java
+++ backend/src/main/java/com/landerer/pokemon/config/PokemonConfig.java
@@ -1,11 +1,21 @@
 package com.landerer.pokemon.config;
 
 import com.landerer.pokemon.logging.RestTemplateLoggingInterceptor;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.ssl.TrustStrategy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
+import javax.net.ssl.SSLContext;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.X509Certificate;
 import java.util.Collections;
 
 @Configuration
@@ -13,9 +23,25 @@ public class PokemonConfig {
 
     @Bean
     @Scope("prototype")
-    public RestTemplate getRestTemplate(RestTemplateLoggingInterceptor interceptor) {
-        RestTemplate template = new RestTemplate();
-        template.setInterceptors(Collections.singletonList(interceptor));
-        return template;
+    public RestTemplate getRestTemplate(RestTemplateLoggingInterceptor interceptor) throws KeyStoreException, NoSuchAlgorithmException, KeyManagementException {
+        TrustStrategy acceptingTrustStrategy = (X509Certificate[] chain, String authType) -> true;
+
+        SSLContext sslContext = org.apache.http.ssl.SSLContexts.custom()
+                .loadTrustMaterial(null, acceptingTrustStrategy)
+                .build();
+
+        SSLConnectionSocketFactory csf = new SSLConnectionSocketFactory(sslContext);
+
+        CloseableHttpClient httpClient = HttpClients.custom()
+                .setSSLSocketFactory(csf)
+                .build();
+
+        HttpComponentsClientHttpRequestFactory requestFactory =
+                new HttpComponentsClientHttpRequestFactory();
+
+        requestFactory.setHttpClient(httpClient);
+        RestTemplate restTemplate = new RestTemplate(requestFactory);
+        restTemplate.setInterceptors(Collections.singletonList(interceptor));
+        return restTemplate;
     }
 }
